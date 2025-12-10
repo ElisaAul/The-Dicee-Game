eeb1d574b9bbfb68f8c6e02580e5d48eccbec8e4
electric_lisa <138820330+electriclisa@users.noreply.github.com>
2025-12-10 22:26:47 +0100

    added functionality to roll dice images on button

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Button Animation on Click
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('button');
+  const imgs = document.querySelectorAll('.dice');
+
   if (!btn) return;
+
   btn.addEventListener('click', () => {
     btn.classList.add('is-clicked');
-    setTimeout(() => btn.classList.remove('is-clicked'), 250);
+
+    imgs.forEach(img => {
+  const roll = getRandom();
+  img.src = `images/dice${roll}.png`;
+});
+
   });
 });
+// Random Number
+function getRandom() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+
