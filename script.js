c8cbb75e890ba7aa1ed812cce52e19da2629141b
electric_lisa <138820330+ElisaAul@users.noreply.github.com>
2025-12-11 18:11:40 +0100

    added functionality to highlight winner dice and updated button style

--- script.js
+++ script.js
@@ -2,17 +2,51 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.querySelector('button');
   const imgs = document.querySelectorAll('.dice');
+  const playerCards = document.querySelectorAll('.player-card');
+  const resultHeading = document.querySelector('h2');
 
   if (!btn) return;
 
   btn.addEventListener('click', () => {
     btn.classList.add('is-clicked');
 
-    imgs.forEach(img => {
-  const roll = getRandom();
-  img.src = `images/dice${roll}.png`;
-});
+    // remove Winner Class from previous round
+    playerCards.forEach(card => card.classList.remove('winner'));
 
+    const rolls = [];
+    imgs.forEach((img, index) => {
+      const roll = getRandom();
+      rolls.push(roll);
+      img.src = `images/dice${roll}.png`;
+    });
+
+    //max Roll to determine Winner
+    const maxRoll = Math.max(...rolls);
+
+    const winners = [];
+    rolls.forEach((roll, index) => {
+      if (roll === maxRoll) {
+        playerCards[index].classList.add('winner');
+        winners.push(index + 1);
+
+        // change Winner Dice Color
+        imgs[index].src = `images/dice${roll}Win.png`;
+      }
+         });
+
+         // More Winners
+      if (winners.length === 1) {
+      resultHeading.textContent = `Player ${winners[0]} wins!`;
+    } else if (winners.length === 2) {
+      resultHeading.textContent = `It's a double!`;
+    } else {
+      resultHeading.textContent = `It’s a triple!`;
+    }
+
+    // timeout to remove the box-shadow effect after animation
+    setTimeout(() => {
+    btn.classList.remove('is-clicked');
+  }, 300);
   });
 });
 // Random Number
